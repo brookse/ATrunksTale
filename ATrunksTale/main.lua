07bec0f9707d5da9bde5b0f13385c8f9fd4799a3
Lyzzi Brooks <lyzzibrooks@Lyzzis-MacBook-Air.local>
2016-01-30 15:13:50 -0600

    can destroy bushes

--- ATrunksTale/main.lua
+++ ATrunksTale/main.lua
@@ -22,6 +22,8 @@ function love.load(arg)
   local bgs = anim8.newGrid(90, 60, babbySprites:getWidth(), babbySprites:getHeight(), 0, 120, 0)
   local g = anim8.newGrid(170, 220, giraffeSprites:getWidth(), giraffeSprites:getHeight(), 0, 0, 0)
 
+  local t = anim8.newGrid(30, 40, grassTuft:getWidth(), grassTuft:getHeight(), 0, 0, 0)
+
   player = {
     spritesheet = babbySprites,
     x = 100,
@@ -40,8 +42,8 @@ function love.load(arg)
       idleRightCarry = anim8.newAnimation(bcs(1,1, 2,1), .5),
       walkLeftCarry = anim8.newAnimation(bcs(7,1, 6,1, 7,1, 5,1), .25),
       walkRightCarry = anim8.newAnimation(bcs(2,1, 3,1, 2,1, 4,1), .25),
-      grabLeft = anim8.newAnimation(bgs(1,1, 2,1, 3,1, 4,1), .25, 'pauseAtEnd'),
-      grabRight = anim8.newAnimation(bgs(8,1, 7,1, 6,1, 5,1), .25, 'pauseAtEnd'),
+      grabRight = anim8.newAnimation(bgs(1,1, 2,1, 3,1, 4,1), .25, 'pauseAtEnd'),
+      grabLeft = anim8.newAnimation(bgs(8,1, 7,1, 6,1, 5,1), .25, 'pauseAtEnd'),
     },
     carrying = 'nothing'
   }
@@ -60,11 +62,16 @@ function love.load(arg)
 
   tuft = {
     spritesheet = grassTuft,
+    animations = {
+      visible = anim8.newAnimation(t(1,1), 1),
+      invisible = anim8.newAnimation(t(2,1), 1)
+    },
     x = 375,
     y = 515,
     width = 30,
     height = 40
   }
+  tuft.animation = tuft.animations.visible
 
 end
 
@@ -131,17 +138,23 @@ function love.keyreleased(key)
 end
 
 function love.mousepressed(x, y, button, istouch)
-  if button == 1 then
+  if button == 2 then
     -- if click is within space of object
     if x > tuft.x and x < tuft.x + tuft.width
     and y > tuft.y and y < tuft.y + tuft.height
+    and math.abs(tuft.x - (player.x + 80)) < 30
     then
       if player.facing == 'left' then
         -- do grab animation
-        player.animation = player.animation.grabLeft
+        player.animation = player.animations.grabLeft
         -- attach object to player
+        player.carrying = grass
+        -- delete tuft
+        tuft.animation = tuft.animations.invisible
       elseif player.facing == 'right' then
-        player.animation = player.animation.grabRight
+        player.animation = player.animations.grabRight
+
+        tuft.animation = tuft.animations.invisible
       end
     end
   end
@@ -170,15 +183,22 @@ function love.mousereleased(x, y, button, istouch)
 end
 
 function love.draw(dt)
-  love.graphics.print({{0, 0, 0, 255}, "< and > OR a and d to move, lmb to carry"}, 100, 650, 0, 2, 2)
   love.graphics.draw(background, 0, 200)
+  love.graphics.print({{0, 0, 0, 255}, "< and > OR a and d to move, lmb to carry"}, 100, 650, 0, 2, 2)
   love.graphics.draw(backGrass, 0, 400)
   love.graphics.draw(backGrass, backGrass:getWidth(), 400)
   player.animation:draw(player.spritesheet, player.x, player.y)
+  if player.carrying == 'nothing' then else
+    love.graphics.draw(grass, player.x, player.y)
+  end
   giraffe.animation:draw(giraffe.spritesheet, giraffe.x, giraffe.y)
   love.graphics.draw(ground, 0, 540)
   love.graphics.draw(groundPool, ground:getWidth(), 540)
   love.graphics.draw(ground, ground:getWidth() + groundPool:getWidth(), 540)
-  love.graphics.draw(tuft.spritesheet, tuft.x, tuft.y)
+  tuft.animation:draw(tuft.spritesheet, tuft.x, tuft.y)
+
+  love.graphics.push()
+  love.graphics.translate(-player.x, 0)
+  love.graphics.pop()
 
 end
